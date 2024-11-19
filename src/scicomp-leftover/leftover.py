18abed462b1e039cda8fd4e2417d393f5cd00452
Michael Gutteridge <michael.gutteridge@gmail.com>
2024-11-19 07:21:12 -0800

    PEP fixes

--- src/scicomp-leftover/leftover.py
+++ src/scicomp-leftover/leftover.py
@@ -13,12 +13,12 @@ from random import randint
 crier = logging.getLogger('crybaby')
 crier.setLevel(logging.DEBUG)
 syslog = logging.handlers.SysLogHandler(
-    address = '/dev/log',
+    address='/dev/log',
     facility='daemon'
 )
-crier.addHandler( syslog )
+crier.addHandler(syslog)
 
-crier.info( 'INFO: leftover starting' )
+crier.info('INFO: leftover starting')
 
 # Sleep for a random number of seconds to keep the leftovers on all the nodes
 # from dogpiling on the Slurm controller
@@ -34,7 +34,7 @@ protected_users = []
 with open('/etc/passwd', 'r') as passwd:
     entry = passwd.readline()
     while entry:
-        (user,pw,uid,gid,grp,home,shell) = entry.split(':')
+        (user, pw, uid, gid, grp, home, shell) = entry.split(':')
         if int(uid) < 1000:
             protected_users.append(user)
             crier.debug(
@@ -54,7 +54,7 @@ crier.debug(
 # find all the nodes configured on this host when
 # multiple slurmd's are running
 
-cmd = [ 'scontrol', 'show', 'aliases' ]
+cmd = ['scontrol', 'show', 'aliases']
 try:
     result = subprocess.run(cmd, capture_output=True, text=True)
 except subprocess.CalledProcessError as err:
@@ -77,7 +77,7 @@ crier.debug(
     'DEBUG: set node names to \'%s\'', nodenames
 )
 
-cmd = [ 'squeue', '-a', '-h', '-w', nodenames, '-o', '%u']
+cmd = ['squeue', '-a', '-h', '-w', nodenames, '-o', '%u']
 
 time_squeue_start = time()
 
@@ -104,45 +104,42 @@ if squeue_elapsed > 2:
     sys.exit(0)
 
 valid_users = set(result.stdout.rstrip('\n').split('\n'))
-valid_users = valid_users.union( protected_users )
+valid_users = valid_users.union(protected_users)
 
-cmd = [ 'ps', '--no-headers', '-e', '-o', 'user' ]
+cmd = ['ps', '--no-headers', '-e', '-o', 'user']
 result = subprocess.run(cmd, capture_output=True, text=True)
 running_users = set(result.stdout.rstrip('\n').split('\n'))
 
-invalid_users = running_users.difference( valid_users )
+invalid_users = running_users.difference(valid_users)
 
-if len( invalid_users ) == 0:
+if len(invalid_users) == 0:
     crier.debug('DEBUG: leftover found no users with invalid jobs,' +
-                'len(invalid_users)=%s', len(invalid_users) )
+                'len(invalid_users)=%s', len(invalid_users))
     exit
 
 for u in invalid_users:
-    crier.info( 'killing processes belonging to user %s', u )
-    cmd = [ 'pkill', '-TERM', '-u', u ]
+    crier.info('killing processes belonging to user %s', u)
+    cmd = ['pkill', '-TERM', '-u', u]
     try:
         result = subprocess.check_output(cmd, stderr=subprocess.STDOUT)
     except subprocess.CalledProcessError as err:
-        crier.debug( 'pkill exited with %s %s', err.returncode, err.output )
-        crier.error( 'ERROR: pkill could not TERM processes from %s', u )
+        crier.debug('pkill exited with %s %s', err.returncode, err.output)
+        crier.error('ERROR: pkill could not TERM processes from %s', u)
         continue
 
-    sleep( 8 )
+    sleep(8)
 
-    cmd = [ 'echo', 'pkill', '-KILL', '-u', u ]
+    cmd = ['echo', 'pkill', '-KILL', '-u', u]
     try:
         result = subprocess.check_output(cmd, stderr=subprocess.STDOUT)
     except subprocess.CalledProcessError as err:
         if err.returncode == 1:
-            crier.debug( "DEBUG: pkill exited non-zero," +
-                        "no processes left to kill" )
+            crier.debug("DEBUG: pkill exited non-zero," +
+                        "no processes left to kill")
             continue
         else:
             crier.debug(
                 'DEBUG: pkill exited with %s %s', err.returncode, err.output
             )
-            crier.error( 'ERROR: pkill could not KILL processes from %s', u )
+            crier.error('ERROR: pkill could not KILL processes from %s', u)
             continue
-
-
-
